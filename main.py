8db8bcd84539c39718dd518eaa2691f9abeac061
大村俊輔 <oomurashunsuke@oomurashunsukenoMacBook-Air.local>
2025-03-14 16:38:07 +0900

    add cores

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import joblib
 import numpy as np
 import os
 from google.cloud import storage
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 # Cloud Storage からモデルをロードする関数
 def load_model():
